d382e6cc10656be4ab2043ca992f99f15530d12d
dan_gwn <danielgawne@kubrickgroup.com>
2021-02-09 14:19:11 +0000

    flask app

--- app/app.py
+++ app/app.py
@@ -1,2 +1,56 @@
+'''
+Author: Dan Gawne
+Date: 2021-02-09
+'''
+
 from flask import Flask, request
 import waitress
+import json
+
+from src import get_data
+
+#--------------------------------------------------------------------------------------------------
+# Error Message
+#--------------------------------------------------------------------------------------------------
+def error_msg(msg : str):
+    return json.dumps({'error' : msg})
+
+#--------------------------------------------------------------------------------------------------
+# App
+#--------------------------------------------------------------------------------------------------
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to the API'
+
+@app.route('sp-data', methods=['POST'])
+def get_sp_data():
+    try:
+        data = json.loads(request.data)
+        if data == None:
+            return error_msg('Problem with loading data...')
+        
+        ticker = data.get('ticker', 'unknown_to_me')
+        if ticker == 'unknown_to_me':
+            return error_msg('Problem with loading ticker...')
+
+        start      = data.get('start', None)
+        end        = data.get('end', None)
+        proxy      = data.get('proxy', None)
+        return_all = data.get('return_all', False)
+
+        return get_data.get_json(
+            ticker = ticker,
+            start = start,
+            end = end,
+            proxy = proxy,
+            return_all = return_all
+        )
+
+    except:
+        return error_msg('Something went wrong...')
+    
+
+if __name__ == '__main__':
+    waitress.serve(app, host='0.0.0.0', port=6000)
