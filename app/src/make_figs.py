05645840c2f44cf48a00b5837f29897f1bca9cc6
dan_gwn <danielgawne@kubrickgroup.com>
2021-02-09 16:53:37 +0000

    basic graph plotter

--- app/src/make_figs.py
+++ app/src/make_figs.py
@@ -21,4 +21,22 @@ def create_day_range(
         d += dt.timedelta(days=1)
     return dates
 
+def plot_to_html(fig):
+    return mpld3.fig_to_html(fig)
 
+# TODO:
+#  Add dates to plot - having 3/4 dates on the bottom at regular intervals
+def create_raw_plot(
+    data,
+    ticker = None, currency = None,
+    start = None, end = None,
+):
+    fig, ax = plt.subplots(figsize=(12,6))
+    
+    ax.plot(data, color='green', linewidth='2.5')
+    ax.set_ylabel('Adjusted Close Price')
+
+    title = (f'{ticker.upper()} ' if ticker is not None else '') + (f'({currency})' if currency is not None else '')
+    ax.set_title(title)
+    
+    return fig
